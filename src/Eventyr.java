4fc0ff9efcdc08e2de3817eb7611f271633f6edf
jonathan <jonathan@my-macbook-pro.eduroam.wireless.dtu.dk>
2018-02-15 14:02:50 +0100

    Version 2: Changes made to names. Scanner included.
    Version 3:
    - Added string to accept new names.

--- src/Eventyr.java
+++ src/Eventyr.java
@@ -5,11 +5,14 @@ public class Eventyr
 	public static void main(String[] arg)
 	{
                 java.util.Scanner tastatur = new java.util.Scanner(System.in);
-                System.out.println("Hvor mange personer skal indgå i historien?");
-                //int antalPersoner = tastatur.nextInt();
-               // tastatur.nextInt();
+                //String s1;
+                System.out.println("Indtast personers navne: ");
+                String person1 = tastatur.toString();
+                String s1 = tastatur.next();
+                
+                
 		ArrayList<String> personer = new ArrayList<String>(); // liste af strenge
-		personer.add("John");
+		personer.add(" "+ s1);
 		personer.add("Henning");
 		personer.add("Troels");
 
@@ -17,6 +20,9 @@ public class Eventyr
 		handlinger.add("slikker sig om munden");
 		handlinger.add("får en idé!");
 		handlinger.add("gemmer sig i skoven");
+                handlinger.add("har det skidt");
+                handlinger.add("får det for vildt!");
+                handlinger.add("er ensom");
 
 		for (int i=0; i<5; i++) {
 			int antalPersoner = personer.size(); // antal personer i listen, dvs 3
